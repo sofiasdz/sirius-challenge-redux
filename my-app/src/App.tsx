f9c886af76c14390d565d885c7b68ab9b0f8fc00
Sofia Sidañez <sofia.sidanez@ing.austral.edu.ar>
2022-06-21 15:00:48 -0300

    added table

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {Container} from "@material-ui/core";
+import BasicTable from "./Table";
+
 
 function App() {
   return (
     <div >
         <Container maxWidth="sm">
             <header>Rick and Morty Characters</header>
+            <BasicTable></BasicTable>
         </Container>
     </div>
   );
