57dd1983c7d07b8340da82e9ccde8f2a2b2959cf
Sofia Sidañez <sofia.sidanez@ing.austral.edu.ar>
2022-06-21 15:12:13 -0300

    changed variables

--- my-app/src/Table.tsx
+++ my-app/src/Table.tsx
@@ -14,16 +14,15 @@ const useStyles = makeStyles({
     },
 });
 
-function createData(name: string, calories: number, fat: number, carbs: number, protein: number) {
-    return { name, calories, fat, carbs, protein };
+function createData(name: string, status: string, specie: string, gender: string, episodes: number, detail:string) {
+    return { name, status, specie, gender, episodes,detail };
 }
 
 const rows = [
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
+    createData('Rick', 'dead', 'human', 'male', 1.0,'cranky'),
+    createData('Rick', 'dead', 'human', 'male', 1.0,'cranky'),
+    createData('Rick', 'dead', 'human', 'male', 1.0,'cranky'),
+
 ];
 
 export default function BasicTable() {
@@ -34,23 +33,23 @@ export default function BasicTable() {
             <Table className={classes.table} aria-label="simple table">
                 <TableHead>
                     <TableRow>
-                        <TableCell>Dessert (100g serving)</TableCell>
-                        <TableCell align="right">Calories</TableCell>
-                        <TableCell align="right">Fat&nbsp;(g)</TableCell>
-                        <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                        <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                        <TableCell>Name</TableCell>
+                        <TableCell align="right">Status</TableCell>
+                        <TableCell align="right">Specie</TableCell>
+                        <TableCell align="right">Gender</TableCell>
+                        <TableCell align="right">Episodes</TableCell>
+                        <TableCell align="right">detail</TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
                     {rows.map((row) => (
                         <TableRow key={row.name}>
-                            <TableCell component="th" scope="row">
-                                {row.name}
-                            </TableCell>
-                            <TableCell align="right">{row.calories}</TableCell>
-                            <TableCell align="right">{row.fat}</TableCell>
-                            <TableCell align="right">{row.carbs}</TableCell>
-                            <TableCell align="right">{row.protein}</TableCell>
+                            <TableCell align="right">{row.name}</TableCell>
+                            <TableCell align="right">{row.status}</TableCell>
+                            <TableCell align="right">{row.specie}</TableCell>
+                            <TableCell align="right">{row.gender}</TableCell>
+                            <TableCell align="right">{row.episodes}</TableCell>
+                            <TableCell align="right">{row.detail}</TableCell>
                         </TableRow>
                     ))}
                 </TableBody>
