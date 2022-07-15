fef7026dc2270460d5e09adf523a6b9f85a3083d
Sofia Sidañez <sofia.sidanez@ing.austral.edu.ar>
2022-07-15 16:44:30 -0300

    error when sending episode request

--- my-app/src/redux/reducers/episodeReducer.tsx
+++ my-app/src/redux/reducers/episodeReducer.tsx
@@ -4,12 +4,13 @@ import {EPISODE_ERROR, EPISODE_REQUEST, EPISODE_RESPONSE} from "../actions/episo
 const initialState = {
     episodes:[],
     status:'',
+    episodeUrl:''
 };
 
-const episodeReducer = (state= initialState,action: {type: string, response:any,episodes:number[] }) =>{
+const episodeReducer = (state= initialState,action: {type: string, response:any,episodeUrl:string,episodes:number[] }) =>{
     switch(action.type){
         case EPISODE_REQUEST:
-            return  {... state,status: 'loading', episodes: action.episodes}
+            return  {... state,status: 'loading', url: action.episodeUrl}
         case EPISODE_RESPONSE:
             return  {... state,status: 'idle', episodes: action.response}
         case EPISODE_ERROR:
