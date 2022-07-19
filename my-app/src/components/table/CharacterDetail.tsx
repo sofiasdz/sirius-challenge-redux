d6642d2a0df1ed45a407a10fef29f9f1722f57e1
Sofia Sidañez <sofia.sidanez@ing.austral.edu.ar>
2022-07-19 15:46:17 -0300

    Trying to fix issue where clicking anywhere on character details modal makes it close

--- my-app/src/components/table/CharacterDetail.tsx
+++ my-app/src/components/table/CharacterDetail.tsx
@@ -1,10 +1,5 @@
-
 import React from 'react';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import {CharacterType, EpisodeType} from "../../Types/Types";
-import {DialogContentText, TextField} from "@material-ui/core";
+import {CharacterType} from "../../Types/Types";
 import {createStyles, makeStyles, Theme} from "@material-ui/core/styles";
 import {MyTheme} from "../../config/Theme";
 
@@ -14,12 +9,15 @@ const useFormTextStyles = makeStyles({
         fontSize: 15
     }
 });
-const useTextFieldStyles = makeStyles({
+
+const useFormTextFieldStyles = makeStyles({
     root: {
-        background: MyTheme.palette.primary.contrastText,
-        borderRadius: 4,
+        backgroundColor: MyTheme.palette.primary.contrastText,
+        color: MyTheme.palette.primary.dark,
+        fontSize: 15,
         width: 350,
-        pointerEvents: 'none'
+        borderRadius: 4,
+        padding: 10,
 
 
     }
@@ -67,114 +65,67 @@ type Props = {
 
 }
 
-const CharacterDetail = (props:Props) => {
-    const {character}=props
-    const classesTextField = useTextFieldStyles();
+const CharacterDetail = (props: Props) => {
+    const {character} = props
     const classesDialogContainer = useDialogContainerStyles();
     const classesFormText = useFormTextStyles();
+    const classesFormTextField = useFormTextFieldStyles();
     const classesImg = useImgStyles();
     const classesForm = useFormStyles();
 
 
-
-
-
-
     return (
         <div>
-        <div className={classesDialogContainer.root}>
-        <img className={classesImg.root} src={character.image}/>
-    </div>
+            <div className={classesDialogContainer.root}>
+                <img className={classesImg.root} src={character.image}/>
+            </div>
 
 
-    <form className={classesForm.root} noValidate
-          autoComplete="off" >
+            <form className={classesForm.root} noValidate
+                  autoComplete="off">
 
-        <text
-            className={classesFormText.root}>Name
-        </text>
+                <text
+                    className={classesFormText.root}>Name
+                </text>
 
-        <TextField id="standard-read-only-input"
+                <text
+                    className={classesFormTextField.root}>{character.name}
+                </text>
+                <text
+                    className={classesFormText.root}>Code
+                </text>
+                <text
+                    className={classesFormTextField.root}>{character.id}
+                </text>
 
-                   defaultValue={character.name}
-                   className={classesTextField.root}
-                   inputProps={{readOnly: true}}
-                   variant="outlined"
-        />
-        <text
-            className={classesFormText.root}>Code
-        </text>
-        <TextField id="standard-read-only-input"
+                <text className={classesFormText.root}>Air
+                    Date
+                </text>
+                <text
+                    className={classesFormTextField.root}>{character.created}
+                </text>
+                <text className={classesFormText.root}>Type
+                </text>
+                <text
+                    className={classesFormTextField.root}>{character.type}
+                </text>
+                <text className={classesFormText.root}>Gender
+                </text>
+                <text
+                    className={classesFormTextField.root}>{character.gender}
+                </text>
+                <text className={classesFormText.root}>Origin
+                </text>
+                <text
+                    className={classesFormTextField.root}>{character.origin.name}
+                </text>
+                <text className={classesFormText.root}>Location
+                </text>
+                <text
+                    className={classesFormTextField.root}>{character.location.name}
+                </text>
 
-                   defaultValue={character.id}
-                   className={classesTextField.root}
-                   InputProps={{
-                       readOnly: true,
-                   }}
-                   variant="outlined"
-
-        />
-        <text className={classesFormText.root}>Air
-            Date
-        </text>
-        <TextField id="standard-read-only-input"
-                   defaultValue={character.created}
-                   InputProps={{
-                       readOnly: true,
-                   }} variant="outlined"
-                   className={classesTextField.root}/>
-        <text className={classesFormText.root}>Type
-        </text>
-        <TextField id="standard-read-only-input"
-
-                   defaultValue={character.type}
-                   InputProps={{
-                       readOnly: true,
-                   }}
-
-                   variant="outlined"
-                   className={classesTextField.root}
-        />
-        <text className={classesFormText.root}>Gender
-        </text>
-        <TextField id="standard-read-only-input"
-
-                   defaultValue={character.gender}
-                   InputProps={{
-                       readOnly: true,
-                   }}
-
-                   variant="outlined"
-                   className={classesTextField.root}
-        />
-        <text className={classesFormText.root}>Origin
-        </text>
-        <TextField id="standard-read-only-input"
-
-                   defaultValue={character.origin.name}
-                   InputProps={{
-                       readOnly: true,
-                   }}
-
-                   className={classesTextField.root}
-                   variant="outlined"
-
-        />
-        <text className={classesFormText.root}>Location
-        </text>
-        <TextField id="standard-read-only-input"
-                   defaultValue={character.location.name}
-                   InputProps={{
-                       readOnly: true,
-                   }}
-
-                   variant="outlined"
-                   className={classesTextField.root}
-
-        />
-
-
-    </form>
+            </form>
         </div>
 
     );
